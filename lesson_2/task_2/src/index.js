9c6367a1c59f000a7c3e37253c5e58f6fd161601
shema1 <shymechko.m.a@gmail.com>
2020-02-19 19:58:09 +0200

    commit

--- lesson_2/task_2/src/index.js
+++ lesson_2/task_2/src/index.js
@@ -2,10 +2,10 @@ import './index.scss'
 import store, {increment,decrement,reset} from './store'
 
 
-const resultElem = document.querySelector('.counter__result')
-const incrementBtn = document.querySelector('[data-action="increment"]')
-const resetBtn = document.querySelector('[data-action="reset"]')
-const decrementBtn = document.querySelector('[data-action="decrement"]')
+const resultElem = document.querySelector('.counter__result');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const resetBtn = document.querySelector('[data-action="reset"]');
+const decrementBtn = document.querySelector('[data-action="decrement"]');
 
 const onIncrement = () =>{
  store.dispatch(increment())
