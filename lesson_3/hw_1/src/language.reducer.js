4e3cdfcc63380ad87952d786ab71d7931c467094
shema1 <shymechko.m.a@gmail.com>
2020-02-21 16:10:13 +0200

    сщььше

--- lesson_3/hw_1/src/language.reducer.js
+++ lesson_3/hw_1/src/language.reducer.js
@@ -5,9 +5,7 @@ import { SELECT_LANGUAGE } from "./language.actions"
 const languageReducer = (state = "en", action) => {
     switch (action.type) {
         case SELECT_LANGUAGE:
-            return{
-                language: action.payload.lang
-            }
+            return action.payload.lang
         default:
             return state
     }
--- lesson_3/hw_1/src/user.reducer.js
+++ lesson_3/hw_1/src/user.reducer.js
@@ -5,13 +5,10 @@ import { ADD_USER, REMOVE_USER } from "./user.actions"
 const usersReducer = (state = null, action) => {
     switch (action.type) {
         case ADD_USER:
-            return {
-                user: action.payload.userData
-            }
+            return  action.payload.userData
         case REMOVE_USER:
-            return {
-                user: null
-            }
+            return  null
+
         default:
             return state
     }
