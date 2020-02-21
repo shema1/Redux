941d62a357751bf75966f29a90d389ed26a4573e
shema1 <shymechko.m.a@gmail.com>
2020-02-21 14:42:38 +0200

    commit

--- lesson_3/hw_1/src/index.js
+++ lesson_3/hw_1/src/index.js
@@ -1,9 +1,9 @@
 import store from './store'
-import { changeLanguage } from './language.actions'
+import { setLanguage } from './language.actions'
 import { addProduct, deleteProduct } from './cart.actions'
 import { setUser, removeUser } from './user.actions'
 
-store.dispatch(changeLanguage("ua"))
+store.dispatch(setLanguage("ua"))
 store.dispatch(addProduct({ id: 76, name: "milk" }))
 store.dispatch(addProduct({ id: 56, name: "meat" }))
 store.dispatch(deleteProduct(76))
--- lesson_3/hw_1/src/language.actions.js
+++ lesson_3/hw_1/src/language.actions.js
@@ -3,7 +3,7 @@ export const SELECT_LANGUAGE = "SELECT_LANGUAGE"
 
 
 
-export const changeLanguage=(lang)=>{
+export const setLanguage=(lang)=>{
     return {
         type: SELECT_LANGUAGE,
         payload:{
